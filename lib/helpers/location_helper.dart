0a14a3dd2cb6847a9ae98a8284259f5a47c7b2f5
Darshan Satya <darshans1989@gmail.com>
2020-01-22 21:39:00 -0500

    Update location_helper.dart

--- lib/helpers/location_helper.dart
+++ lib/helpers/location_helper.dart
@@ -1,4 +1,4 @@
-const GOOGLE_API_KEY = "AIzaSyBHbS1OqlccjXyfCzpq_f6604kLEzZwXF4";
+const GOOGLE_API_KEY = "";
 
 class LocationHelper {
   static String getLocationPreviewImage({double latitude, double longitude}) {
