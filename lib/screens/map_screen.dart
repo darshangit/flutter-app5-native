0365b084aa7679f12d56e7b3dec35dfc2eb525f8
Darshan Satya <darshans1989@gmail.com>
2020-01-26 12:18:19 -0500

    Picking a location

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app_5_native/models/place.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+
 class MapsScreen extends StatefulWidget {
   final PlaceLocation initialLocation;
   final bool isSelecting;
@@ -15,17 +16,42 @@ class MapsScreen extends StatefulWidget {
 }
 
 class _MapsScreenState extends State<MapsScreen> {
+  LatLng _pickedLocation;
+
+  void _selectLocation(LatLng position) {
+    setState(() {
+      _pickedLocation = position;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Your Map'),
+        actions: <Widget>[
+          if (widget.isSelecting)
+            IconButton(
+              icon: Icon(Icons.check),
+              onPressed: _pickedLocation == null
+                  ? null
+                  : () {
+                      Navigator.of(context).pop(_pickedLocation);
+                    },
+            )
+        ],
       ),
       body: GoogleMap(
         initialCameraPosition: CameraPosition(
             target: LatLng(widget.initialLocation.latitude,
                 widget.initialLocation.longitude),
             zoom: 16),
+        onTap: widget.isSelecting ? _selectLocation : null,
+        markers: _pickedLocation == null
+            ? null
+            : {
+                Marker(markerId: MarkerId('m1'), position: _pickedLocation),
+              },
       ),
     );
   }
--- lib/widgets/place_input.dart
+++ lib/widgets/place_input.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app_5_native/helpers/location_helper.dart';
 import 'package:flutter_app_5_native/screens/map_screen.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
 
 class LocationInput extends StatefulWidget {
@@ -22,15 +23,18 @@ class _LocationInputState extends State<LocationInput> {
   }
 
   Future<void> _selectOnMap() async {
-    final selectedLocation = await Navigator.of(context).push(MaterialPageRoute(
-        fullscreenDialog: true,
-        builder: (ctx) => MapsScreen(
-              isSelecting: true,
-            )));
+    final selectedLocation =
+        await Navigator.of(context).push<LatLng>(MaterialPageRoute(
+            fullscreenDialog: true,
+            builder: (ctx) => MapsScreen(
+                  isSelecting: true,
+                )));
 
     if (selectedLocation == null) {
       return;
     }
+
+    print(selectedLocation.latitude);
   }
 
   @override
